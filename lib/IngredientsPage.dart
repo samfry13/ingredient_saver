a0ee0623844901f6107648022d7804d0a33eb6ed
Samuel Fry <samuel.fry13@gmail.com>
2020-04-04 15:09:45 -0600

    Finished Ingredients and Recipes Pages

--- lib/IngredientsPage.dart
+++ lib/IngredientsPage.dart
@@ -2,16 +2,6 @@ import 'package:flutter/material.dart';
 
 class IngredientsPage extends StatefulWidget {
   IngredientsPage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -19,68 +9,165 @@ class IngredientsPage extends StatefulWidget {
 }
 
 class _IngredientsPageState extends State<IngredientsPage> {
-  int _counter = 0;
+  bool isSearching = false;
+  TextEditingController textEditingController = new TextEditingController();
+  FocusNode focusNode = new FocusNode();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  void initState() {
+    super.initState();
+    textEditingController.addListener(() {
+      this.setState(() { isSearching = textEditingController.text != ""; });
     });
+  }
+
+  void dispose() {
+    focusNode.unfocus();
+    focusNode.dispose();
+    textEditingController.clearComposing();
+    textEditingController.clear();
+    textEditingController.dispose();
+    super.dispose();
+  }
+
+  Widget buildSearchView() {
+    if (isSearching != null && isSearching) {
+      return Expanded(
+        child: ListView.builder(
+            itemCount: 24,
+            itemBuilder: (BuildContext context, int index) {
+              if (index == 0) {
+                return Padding(
+                  padding: EdgeInsets.fromLTRB(16, 32, 16, 16),
+                  child: Text("Results", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),),
+                );
+              }
+
+              return InkWell(
+                onTap: () {},
+                child: ListTile(
+                  title: Text("Ingredient"),
+                  subtitle: Text("Expires on 01/01/2021"),
+                  trailing: Text("5 lbs"),
+                ),
+              );
+            }
+        ),
+      );
+    }
+    else {
+      return Expanded(
+        child: ListView(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.fromLTRB(16, 32, 16, 16),
+              child: Text("Expiring Soon", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),),
+            ),
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text("Ingredient"),
+                subtitle: Text("Expires on 01/01/2021"),
+                trailing: Text("5 lbs"),
+              ),
+            ),
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text("Ingredient"),
+                subtitle: Text("Expires on 01/01/2021"),
+                trailing: Text("5 lbs"),
+              ),
+            ),
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text("Ingredient"),
+                subtitle: Text("Expires on 01/01/2021"),
+                trailing: Text("5 lbs"),
+              ),
+            ),
+            Divider(),
+            Padding(
+              padding: EdgeInsets.all(16),
+              child: Text("Categories", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),),
+            ),
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text("Category"),
+                trailing: Icon(Icons.keyboard_arrow_right),
+              ),
+            ),
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text("Category"),
+                trailing: Icon(Icons.keyboard_arrow_right),
+              ),
+            ),
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text("Category"),
+                trailing: Icon(Icons.keyboard_arrow_right),
+              ),
+            ),
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text("Category"),
+                trailing: Icon(Icons.keyboard_arrow_right),
+              ),
+            ),
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text("Category"),
+                trailing: Icon(Icons.keyboard_arrow_right),
+              ),
+            ),
+          ],
+        ),
+      );
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
+            child: TextField(
+              focusNode: focusNode,
+              controller: textEditingController,
+              decoration: InputDecoration(
+                labelText: "Search",
+                prefixIcon: Icon(Icons.search),
+                suffixIcon: isSearching ? IconButton(
+                  icon: Icon(Icons.close),
+                  onPressed: () {
+                    textEditingController.clearComposing();
+                    textEditingController.clear();
+                    focusNode.unfocus();
+                  },
+                ) : null
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
+          ),
+          buildSearchView(),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () {},
         tooltip: 'Increment',
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
--- lib/RecipesPage.dart
+++ lib/RecipesPage.dart
@@ -3,15 +3,6 @@ import 'package:flutter/material.dart';
 class RecipesPage extends StatefulWidget {
   RecipesPage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -19,18 +10,6 @@ class RecipesPage extends StatefulWidget {
 }
 
 class _RecipesPageState extends State<RecipesPage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -46,38 +25,89 @@ class _RecipesPageState extends State<RecipesPage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(itemCount: 8, itemBuilder: (BuildContext context, int index) {
+          return Padding(
+            padding: const EdgeInsets.fromLTRB(0, 0, 0, 16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Container(
+                  height: 240,
+                  width: 160,
+                  child: Card(
+                    color: Theme.of(context).accentColor,
+                    child: InkWell(
+                      onTap: () {},
+                      child: Column(
+                        children: <Widget>[
+                          Expanded(
+                            flex: 11,
+                            child: FittedBox(fit: BoxFit.cover, child: Image.asset("assets/recipe.jpg")),
+                          ),
+                          Expanded(
+                            flex: 4,
+                            child: Padding(
+                              padding: EdgeInsets.fromLTRB(8, 8, 8, 0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Padding(
+                                    padding: EdgeInsets.fromLTRB(0, 0, 0, 8),
+                                    child: Text("Gourmet Nachos", style: TextStyle(fontSize: 14)),
+                                  ),
+                                  Text("3/5 Ingredients Owned", style: TextStyle(fontSize: 12))
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 240,
+                  width: 160,
+                  child: Card(
+                    color: Theme.of(context).accentColor,
+                    child: InkWell(
+                      onTap: () {},
+                      child: Column(
+                        children: <Widget>[
+                          Expanded(
+                            flex: 11,
+                            child: FittedBox(fit: BoxFit.cover, child: Image.asset("assets/recipe.jpg")),
+                          ),
+                          Expanded(
+                            flex: 4,
+                            child: Padding(
+                              padding: EdgeInsets.fromLTRB(8, 8, 8, 0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Padding(
+                                    padding: EdgeInsets.fromLTRB(0, 0, 0, 8),
+                                    child: Text("Gourmet Nachos", style: TextStyle(fontSize: 14)),
+                                  ),
+                                  Text("3/5 Ingredients Owned", style: TextStyle(fontSize: 12))
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
+          );
+        }),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () {},
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
